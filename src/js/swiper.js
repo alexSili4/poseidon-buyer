26225e28febf29bf16538f072886db853bd18db2
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-08 16:00:04 +0300

    update reviews styles

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,8 +7,23 @@ const reviewsSliderMobile = new Swiper('.js-reviews-slider-mobile', {
   modules: [Navigation, Pagination],
   direction: 'horizontal',
   centeredSlides: true,
+  loop: true,
   spaceBetween: 8,
   slidesPerView: 1.095,
+  speed: 800,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.js-reviews-slider-btn-next',
+    prevEl: '.js-reviews-slider-btn-prev',
+  },
+});
+
+const reviewsSliderDesk = new Swiper('.js-reviews-slider-desk', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 32,
+  slidesPerView: 3.3,
   speed: 800,
   grabCursor: true,
   navigation: {
