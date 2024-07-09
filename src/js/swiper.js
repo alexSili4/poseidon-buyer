3977ade5349bd12a1ca18436df08eeabd38792c6
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-09 11:24:56 +0300

    ad pagination

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,17 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
+
+const getAboutSliderSectionTitle = (sectionIndex) => {
+  const titles = {
+    0: 'Покупець бачить рекламу продукта на сайті торгової мережі',
+    1: 'Вказує номер телефона',
+    2: 'Отримує SMS з купоном',
+    3: 'Відвідує магазин і отримує товар',
+  };
+
+  return titles[sectionIndex];
+};
 
 const reviewsSlider = new Swiper('.js-reviews-slider', {
   modules: [Navigation, Pagination],
@@ -13,7 +23,7 @@ const reviewsSlider = new Swiper('.js-reviews-slider', {
   speed: 800,
   grabCursor: true,
   breakpoints: {
-    1250: {
+    14400: {
       spaceBetween: 32,
       slidesPerView: 3.65,
       centeredSlides: false,
@@ -26,10 +36,32 @@ const reviewsSlider = new Swiper('.js-reviews-slider', {
 });
 
 const aboutSlider = new Swiper('.js-about-slider', {
-  modules: [Autoplay],
-  speed: 800,
+  modules: [Pagination, Autoplay, EffectFade],
+  spaceBetween: 30,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  speed: 1000,
+  allowTouchMove: false,
   autoplay: {
     delay: 3000,
     pauseOnMouseEnter: true,
+    waitForTransition: false,
+  },
+  pagination: {
+    el: '.about-slider-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      const number = String(index + 1).padStart(2, 0);
+      const aboutSliderSectionTitle = getAboutSliderSectionTitle(index);
+
+      return `
+      <div class="about-slider-pagination-item ${className}">
+        <p class="about-slider-pagination-item-number">${number}</p>
+        <p class="about-slider-pagination-item-title">${aboutSliderSectionTitle}</p>
+      </div>
+      `;
+    },
   },
 });
